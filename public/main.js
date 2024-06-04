7f3cab310294215efa78b73c910c8f5e1221506a
eliza <eliza.lacroes@gmail.com>
2024-06-04 21:33:33 +0200

    comments

--- public/main.js
+++ public/main.js
@@ -7,12 +7,17 @@ const nameInput = document.getElementById("name-input");
 const messageForm = document.getElementById("message-form");
 const messageInput = document.getElementById("message-input");
 
+
 const messageTone = new Audio("/message-tone.mp3");
+
+// Event listener voor submit event -> verstuurd het bericht
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   sendMessage();
 });
+
+// Laat zien hoeveel mensen er online zijn:
 
 socket.on("clients-total", (data) => {
   clientsTotal.innerText = `Online: ${data}`;
@@ -22,20 +27,25 @@ function sendMessage() {
   if (messageInput.value === "") return;
   // console.log(messageInput.value)
   const data = {
-    name: nameInput.value,
-    message: messageInput.value,
-    dateTime: new Date(),
+    name: nameInput.value, // naam van gebruiker uit formulier
+    message: messageInput.value, // bericht uit formulier
+    dateTime: new Date(), // datum van het bericht
   };
   socket.emit("message", data);
-  addMessageToUI(true, data);
-  messageInput.value = "";
+  addMessageToUI(true, data); 
+  messageInput.value = ""; // Leegt text velt nadat het bericht verstuurt is
 }
+
+// geluid dat afspeelt wanneer je het bericht verstuurd
 
 socket.on("chat-message", (data) => {
   // console.log(data)
-  messageTone.play();
+  messageTone.play(); 
   addMessageToUI(false, data);
 });
+
+// zorgt ervoor dat eigen berichten aan de rechterkant staan en berichten van iemand anders aan
+// de linkerkant + automatisch naar beneden scrollen. 
 
 function addMessageToUI(isOwnMessage, data) {
   clearFeedback();
@@ -55,6 +65,8 @@ function addMessageToUI(isOwnMessage, data) {
 function scrollToBottom() {
   messageContainer.scrollTo(0, messageContainer.scrollHeight);
 }
+
+// feedforward & feedback
 
 messageInput.addEventListener("focus", (e) => {
   socket.emit("feedback", {
